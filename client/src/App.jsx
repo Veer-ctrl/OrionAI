f1f21e7ad52ea5c349a18a86105cbf02cecbfbd8
veer <veerprrajput@gmail.com>
2026-06-22 13:44:59 +0530

    Initialize project structure and development environment

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 text-white text-4xl flex items-center justify-center">
+      Tailwind v3 is working!
+    </div>
+  );
+}
+
+export default App;
